50f340df0740d3eb64231431eddbbde4b3f026ff
Mizz1ove <pa990512@naver.com>
2024-03-26 17:09:15 +0900

    Feat : getMember 구현

--- src/main/java/com/nhnacademy/account/user/controller/MemberController.java
+++ src/main/java/com/nhnacademy/account/user/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.nhnacademy.account.user.controller;
 
 import com.nhnacademy.account.user.dto.LoginRequestDto;
+import com.nhnacademy.account.user.dto.LoginResponseDto;
 import com.nhnacademy.account.user.entity.Member;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.nhnacademy.account.user.service.MemberService;
@@ -13,13 +16,14 @@ import com.nhnacademy.account.user.service.MemberService;
 @RestController
 @RequestMapping("/member/login")
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping
-    public ResponseEntity<Member> getMemberIdAndPassword(LoginRequestDto loginRequestDto){
-
-        return ResponseEntity.status(HttpStatus.OK).body(memberService.getMemberIdAndPassword(loginRequestDto));
+    @GetMapping("/{memberId}")
+    public ResponseEntity<LoginResponseDto> getMemberIdAndPassword(@PathVariable("memberId")String memberId){
+        log.info("{}", "here");
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.getMemberIdAndPassword(memberId));
     }
 }
