9323c66607d5509476da6e024febf9ea36114b85
insub2004 <insub2004@naver.com>
2024-03-30 16:33:40 +0900

    Feat : 비밀번호 암호화
    회원 가입 요청 시BCryptPasswordEncoder 를 이용해서 데이터베이스에 암호화된 비밀번호 저장

--- src/main/java/com/nhnacademy/account/user/service/impl/MemberServiceImpl.java
+++ src/main/java/com/nhnacademy/account/user/service/impl/MemberServiceImpl.java
@@ -5,6 +5,7 @@ import com.nhnacademy.account.user.dto.JoinRequestDto;
 import com.nhnacademy.account.user.dto.JoinResponseDto;
 import com.nhnacademy.account.user.dto.LoginInfoResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.nhnacademy.account.user.entity.Member;
 import com.nhnacademy.account.user.repository.MemberRepository;
@@ -17,6 +18,7 @@ import java.util.Objects;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public LoginInfoResponseDto getMemberIdAndPassword(String id) {
@@ -34,7 +36,7 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public JoinResponseDto createMember(JoinRequestDto request) {
       
-        Member member = Member.createMember(request.getId(), request.getPassword());
+        Member member = Member.createMember(request.getId(), passwordEncoder.encode(request.getPassword()));
       
         if (memberRepository.findById(request.getId()).isPresent()) {
             throw new IllegalStateException("already exist " + member.getId());
