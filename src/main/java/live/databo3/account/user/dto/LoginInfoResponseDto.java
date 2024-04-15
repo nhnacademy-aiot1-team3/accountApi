da6f7a6fd58af29516f98012e89c545714f4aac5
insub2004 <insub2004@naver.com>
2024-04-15 11:15:20 +0900

    Fix : LoginInfoResponseDto 변경
    version 1.0.1 Dto 필드 수정

--- src/main/java/live/databo3/account/user/dto/LoginInfoResponseDto.java
+++ src/main/java/live/databo3/account/user/dto/LoginInfoResponseDto.java
@@ -5,17 +5,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 /**
  * 데이터베이스에서 id로 조회한 Member 결과를 담을 response Dto
  * @author insub
- * @version 1.0.0
+ * @version 1.0.1
  */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginInfoResponseDto {
-    private String id;
-    private String password;
-
+    private String memberNumber;
+    private String memberId;
+    private String memberPassword;
+    private String memberEmail;
+    private String role;
+    private String state;
+    private LocalDateTime lastLoginDateTime;
 }
