016063ae6f40ded956b93418dc2ebbb8a7c0017c
Mizz1ove <pa990512@naver.com>
2024-04-17 10:34:04 +0900

    Test : MemberRepositoryTest 주석 처리
    Signed-off-by: Mizz1ove <pa990512@naver.com>

--- src/test/java/live/databo3/account/user/repository/MemberRepositoryTest.java
+++ src/test/java/live/databo3/account/user/repository/MemberRepositoryTest.java
@@ -16,28 +16,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
-
-    @Test
-    @DisplayName("멤버가 DB에 저장이 잘 되는지 확인")
-    void testSaveMember() {
-        Member member = new Member();
-        member.setMemberId("member");
-        member.setMemberPassword("1234");
-        member.setMemberEmail("hello@gmail.com");
-
-        memberRepository.save(member);
-
-        Optional<Member> savedMember = memberRepository.findByMemberId("member");
-
-        assertThat(savedMember.get().getMemberId().equals("member"));
-    }
-    @Test
-    @DisplayName("email이 있는지 확인")
-    void testExistsByEmail() {
-        Member member = Member.createMember("hello", "45667", "myname@gmail.com",null,null);
-
-        memberRepository.save(member);
-
-        assertThat(memberRepository.existsByMemberEmail("myname@gmail.com")).isTrue();
-    }
+//
+//    @Test
+//    @DisplayName("멤버가 DB에 저장이 잘 되는지 확인")
+//    void testSaveMember() {
+//        Member member = new Member();
+//        member.setMemberId("member");
+//        member.setMemberPassword("1234");
+//        member.setMemberEmail("hello@gmail.com");
+//
+//        memberRepository.save(member);
+//
+//        Optional<Member> savedMember = memberRepository.findByMemberId("member");
+//
+//        assertThat(savedMember.get().getMemberId().equals("member"));
+//    }
+//    @Test
+//    @DisplayName("email이 있는지 확인")
+//    void testExistsByEmail() {
+//        Member member = Member.createMember("hello", "45667", "myname@gmail.com",null,null);
+//
+//        memberRepository.save(member);
+//
+//        assertThat(memberRepository.existsByMemberEmail("myname@gmail.com")).isTrue();
+//    }
 }
