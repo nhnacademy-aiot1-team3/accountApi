360d7a81859b16994619923aebbfbf57452343b5
insub2004 <insub2004@naver.com>
2024-04-18 10:56:38 +0900

    Docs : JavaDoc 작성
    Signed-off-by: insub2004 <insub2004@naver.com>

--- src/main/java/live/databo3/account/member/repository/RolesRepository.java
+++ src/main/java/live/databo3/account/member/repository/RolesRepository.java
@@ -3,5 +3,10 @@ package live.databo3.account.member.repository;
 import live.databo3.account.member.entity.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Roles JpaRepository interface
+ * @author insub
+ * @since 1.0.1
+ */
 public interface RolesRepository extends JpaRepository<Roles, Long> {
 }
