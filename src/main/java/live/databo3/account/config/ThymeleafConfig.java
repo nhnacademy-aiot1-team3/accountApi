389d245fac324ffe201f94a9d326458d1d35fda4
insub2004 <insub2004@naver.com>
2024-04-25 17:55:07 +0900

    Feat: email 인증 구현 중
    redis dependency 추가,
    RedisConfig 추가,
    ThymeleafConfig 추가,
    RedisUtil 추가
    
    Signed-off-by: insub2004 <insub2004@naver.com>

---
+++ src/main/java/live/databo3/account/config/ThymeleafConfig.java
@@ -0,0 +1,35 @@
+package live.databo3.account.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public TemplateEngine htmlTemplateEngine() {
+
+        TemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(springResourceTemplateResolver());
+
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver springResourceTemplateResolver() {
+
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setPrefix("classpath:templates/");
+        springResourceTemplateResolver.setSuffix(".html");
+        springResourceTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        springResourceTemplateResolver.setCharacterEncoding("UTF-8");
+        springResourceTemplateResolver.setCacheable(false);
+
+        return springResourceTemplateResolver;
+    }
+
+}
