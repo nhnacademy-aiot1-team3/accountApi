8e2996c02994290ef85130d6b7349d8d4f9f573d
badgelatte <saucy5939@gmail.com>
2024-04-30 17:53:45 +0900

    Feat: OrganizationRepository 수정
    사용하지 않는 import 삭제

--- src/main/java/live/databo3/account/organization/repository/OrganizationRepository.java
+++ src/main/java/live/databo3/account/organization/repository/OrganizationRepository.java
@@ -3,7 +3,5 @@ package live.databo3.account.organization.repository;
 import live.databo3.account.organization.entity.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
 }
