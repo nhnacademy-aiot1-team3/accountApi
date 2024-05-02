7ca53d7a5489fe15a41a133a370395eedc6921c1
badgelatte <saucy5939@gmail.com>
2024-05-02 22:47:09 +0900

    Feat: ModifyOrgsRequest dto 추가
    조직 이름 수정할 경우의 dto 추가

---
+++ src/main/java/live/databo3/account/organization/dto/ModifyOrgsRequest.java
@@ -0,0 +1,10 @@
+package live.databo3.account.organization.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ModifyOrgsRequest {
+    private String organizationName;
+}
