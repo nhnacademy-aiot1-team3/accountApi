94287c6d78b714290d98e516635cba67875e3206
badgelatte <saucy5939@gmail.com>
2024-05-04 16:52:02 +0900

    Feat: builder 추가

--- src/main/java/live/databo3/account/organization/dto/ModifyOrgsRequest.java
+++ src/main/java/live/databo3/account/organization/dto/ModifyOrgsRequest.java
@@ -1,5 +1,6 @@
 package live.databo3.account.organization.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,4 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ModifyOrgsRequest {
     private String organizationName;
+
+    @Builder
+    public ModifyOrgsRequest(String organizationName) {
+        this.organizationName = organizationName;
+    }
 }
