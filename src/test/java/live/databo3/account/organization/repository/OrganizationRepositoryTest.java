f72a58f6955d176881c4f271e2320cff221ec573
badgelatte <saucy5939@gmail.com>
2024-05-09 09:28:27 +0900

    Fix: 오류 수정
    Organization 생성할 때 organizationId를 제외함

--- src/test/java/live/databo3/account/organization/repository/OrganizationRepositoryTest.java
+++ src/test/java/live/databo3/account/organization/repository/OrganizationRepositoryTest.java
@@ -12,31 +12,30 @@ import java.util.Optional;
 @DataJpaTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class OrganizationRepositoryTest {
-//
-//    @Autowired
-//    private OrganizationRepository organizationRepository;
-//
-//    @BeforeEach
-//    void setUp(){
-//        Organization organization = Organization.builder()
-//                .organizationId(1)
-//                .organizationName("nhn 김해")
-//                .gatewaySn("gatewaySn")
-//                .controllerSn("controllerSn")
-//                .build();
-//        organizationRepository.save(organization);
-//    }
-//
-//    @Test
-//    @DisplayName("조직 이름으로 찾기")
-//    void findByOrganizationName() {
-//
-//        Optional<Organization> organizationOptional = organizationRepository.findByOrganizationName("nhn 김해");
-//        log.info("{}", organizationOptional);
-//        Assertions.assertNotNull(organizationOptional.get());
-//        Assertions.assertEquals(1, organizationOptional.get().getOrganizationId());
-//        Assertions.assertEquals("nhn 김해", organizationOptional.get().getOrganizationName());
-//        Assertions.assertEquals("gatewaySn", organizationOptional.get().getGatewaySn());
-//        Assertions.assertEquals("controllerSn", organizationOptional.get().getControllerSn());
-//    }
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @BeforeEach
+    void setUp(){
+        Organization organization = Organization.builder()
+                .organizationName("nhn 김해")
+                .gatewaySn("gatewaySn")
+                .controllerSn("controllerSn")
+                .build();
+        organizationRepository.save(organization);
+    }
+
+    @Test
+    @DisplayName("조직 이름으로 찾기")
+    void findByOrganizationName() {
+
+        Optional<Organization> organizationOptional = organizationRepository.findByOrganizationName("nhn 김해");
+        log.info("{}", organizationOptional);
+        Assertions.assertNotNull(organizationOptional.get());
+        Assertions.assertEquals(1, organizationOptional.get().getOrganizationId());
+        Assertions.assertEquals("nhn 김해", organizationOptional.get().getOrganizationName());
+        Assertions.assertEquals("gatewaySn", organizationOptional.get().getGatewaySn());
+        Assertions.assertEquals("controllerSn", organizationOptional.get().getControllerSn());
+    }
 }
